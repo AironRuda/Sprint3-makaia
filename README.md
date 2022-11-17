d9f2c1c0c72bf21599284c14792e25d0b9566a24
AironRuda <airon.ruda@udea.edu.co>
2022-11-17 12:06:03 -0500

    readme actualizado

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# prueba
-prueba
+# Sprint 3
+
+<ul>En el repositorio se encuentran los siguientes documentos:
+<li>Carpeta “images” en donde se almacenan las imágenes utilizadas</li>
+<li>Carpeta “styles” en donde se almacenan los estilos implementados</li>
+<li>Documento “index.html” que contiene las etiquetas usadas y es el archivo que se debe correr en el navegador para observar el funcionamiento del proyecto. </li>
+<li>Documento “main.js” que contiene la lógica de programación implementada en el proyecto</li>
+<li>Documento “db.json” que contiene la información almacenada e implementada correspondiente al JSON server requerido </li>
+<li>Documento “READ.md” que contiene la descripción del contenido del repositorio y descripción del funcionamiento del proyecto</li>
+<li>Documento “Proyecto Sprint 3.pdf” que contiene los requerimientos del sprint</li>
+<hr>
+
+# Presentado por:
+
+Airon Ruda
+
+# Funcionamiento
+
+Para que la información pueda desplegarse correctamente se debe contar con el JSON-SERVER usando en una consola, el siguiente código:
+
+npm install -g json-server
+
+para la instalación del servicio en general.
+Además, en una consola de tipo cmd, se debe ingresar el siguiente código:
+
+json-server –watch db.json
+
+El proyecto consiste en una página para una inmobiliaria, en el que se presentan propiedades con características almacenadas en un JSON-SERVER, en el encabezado de la página se presenta un buscador en el que se pueden filtrar las propiedades por locación(afueras o centro de la ciudad), tipo de propiedad (casa o apartamento), tipo de disponibilidad de la propiedad (renta o venta) y nombre de propietario
--- main.js
+++ main.js
@@ -1,8 +1,212 @@
-let cards = document.querySelectorAll(".card");
+const getData = async (searchData) => {
+  searchData != undefined
+    ? (API_URL = "http://localhost:3000/data?q=" + searchData)
+    : (API_URL = "http://localhost:3000/data");
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return data;
+  } catch (error) {}
+};
+const cleanField = (field) => {
+  const cardField = document.querySelector(`#${field}`);
+  cardField.innerHTML = "";
+};
+
+const createBasicCards = async (search) => {
+  cleanField("cardField");
+  const data = await getData(search);
+  data.forEach((eachInfo) => {
+    const {
+      img,
+      id,
+      propertyType,
+      businessType,
+      price,
+      location,
+      owner,
+      totalArea,
+      parking,
+      bathRoom,
+      rooms,
+    } = eachInfo;
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.innerHTML = `
+    <div class="card-img">
+        <p class="hide hideId">${id}</p>
+        <img
+        src="${img}"
+        alt=""
+        srcset=""
+        />
+        <p class="propertyType">${propertyType}</p>
+        <p class="businessType">${businessType}</p>
+        <button class="fav" id="favButton">fav</button>
+        <p class="price">${price} M</p>
+    </div>
+    <div class="card-info">
+        <p class="location">${location}</p>
+        <p class="owner">Propietario: ${owner.ownerName}</p>
+        <p class="total-area">${totalArea} sq ft</p>
+        <p class="parking">parking:${parking}</p>
+        <p class="bathRoom">bathroom:${bathRoom}</p>
+        <p class="rooms">rooms:${rooms}</p>
+    </div>
+    `;
+    cardField.appendChild(card);
+  });
+};
+
+const createFavCards = async (favImg) => {
+  // cleanField("favField");
+  const favSelected = document.querySelectorAll(".selected");
+  favSelected.forEach((favInfo) => {
+    console.log(favInfo);
+    const imgDir = favInfo.querySelector(".hideId").textContent;
+    const data = getData("./images/2 featured properties/apartamento1.jpg");
+    console.log(data);
+  });
+};
+createFavCards();
+
+let favSelector = async () => {
+  await createBasicCards();
+  let cards = document.querySelectorAll(".card");
+  cards.forEach((eachCard) => {
+    let favButtons = eachCard.querySelector(".fav");
+    favButtons.addEventListener("click", () => {
+      favButtons.classList.toggle("fav-clicked");
+      eachCard.classList.toggle("selected");
+    });
+  });
+};
+const form = document.querySelector(".form");
+form.addEventListener("submit", (search) => {
+  search.preventDefault();
+  const searchData = search.target.search.value;
+  createBasicCards(searchData);
+  console.log(searchData);
+});
+// favSelector();
+
+/*
+let menu = document.querySelector("#menu");
+
+
 let homeSection = document.querySelector("#content");
 let favSection = document.querySelector("#favs");
-let menu = document.querySelector("#menu");
-//console.log(cards);
+
+const getData = async () => {
+  const API_URL = "http://localhost:3000/data";
+  try {
+    const response = await fetch(API_URL);
+    const data = await response.json();
+    return await data;
+  } catch (error) {}
+};
+console.log(getData());
+
+
+
+const createBasicCards = async (search) => {
+  const cardField = document.querySelector("#cardField");
+  cardField.innerHTML = "";
+  const data = await getData();
+  data.forEach((eachInfo) => {
+    const {
+      img,
+      propertyType,
+      businessType,
+      price,
+      location,
+      owner,
+      totalArea,
+      parking,
+      bathRoom,
+      rooms,
+    } = eachInfo;
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.innerHTML = `
+  <div class="card-img">
+    <img
+      src="${img}"
+      alt=""
+      srcset=""
+    />
+    <p class="propertyType">${propertyType}</p>
+    <p class="businessType">${businessType}</p>
+    <button class="fav">fav</button>
+    <p class="price">${price}</p>
+  </div>
+  <div class="card-info">
+    <p class="location">${location}</p>
+    <p class="owner">${owner.ownerName}</p>
+    <p class="total-area">${totalArea} ft</p>
+    <p class="parking">${parking}</p>
+    <p class="bathRoom">${bathRoom}</p>
+    <p class="rooms">${rooms}</p>
+  </div>
+  `;
+  });
+};
+
+const createBasicCards = async () => {
+  const cardField = document.querySelector("#cardField");
+  cardField.innerHTML = "";
+  const data = await getData();
+  data.forEach((eachInfo) => {
+    const {
+      img,
+      propertyType,
+      businessType,
+      price,
+      location,
+      owner,
+      totalArea,
+      parking,
+      bathRoom,
+      rooms,
+    } = eachInfo;
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+    card.innerHTML = `
+  <div class="card-img">
+    <img
+      src="${img}"
+      alt=""
+      srcset=""
+    />
+    <p class="propertyType">${propertyType}</p>
+    <p class="businessType">${businessType}</p>
+    <button class="fav">fav</button>
+    <p class="price">${price}</p>
+  </div>
+  <div class="card-info">
+    <p class="location">${location}</p>
+    <p class="owner">${owner.ownerName}</p>
+    <p class="total-area">${totalArea} ft</p>
+    <p class="parking">${parking}</p>
+    <p class="bathRoom">${bathRoom}</p>
+    <p class="rooms">${rooms}</p>
+  </div>
+  `;
+  });
+};
+createBasicCards();
+
+
+let favSelector = () => {
+  let cards = document.querySelectorAll(".card");
+  cards.forEach((eachCard) => {
+    let favButtons = eachCard.querySelector(".fav");
+    favButtons.addEventListener("click", () => {
+      favButtons.classList.toggle("fav-clicked");
+      eachCard.classList.toggle("selected");
+    });
+  });
+};
 
 let cleanPage = () => {
   let cleanPage = document.querySelectorAll(".page");
@@ -11,20 +215,7 @@ let cleanPage = () => {
   });
 };
 
-let favSelector = () => {
-  cards.forEach((eachCard) => {
-    let favButtons = eachCard.querySelector(".fav");
-    favButtons.addEventListener("click", () => {
-      favButtons.classList.toggle("fav");
-      favButtons.classList.toggle("fav-clicked");
-
-      eachCard.classList.toggle("selected");
-      //console.log(eachCard);
-      //console.log(document.querySelectorAll(".selected"));
-    });
-  });
-};
-favSelector();
+// favSelector();
 
 menu.addEventListener("click", (selectedArea) => {
   cleanPage();
@@ -40,9 +231,10 @@ menu.addEventListener("click", (selectedArea) => {
         break;
     }
   }
-  /* 
-  
-    switch (selected.className) {
+});
+
+
+switch (selected.className) {
       case "homeSection":
         homeSection.remove("hide");
         break;
@@ -52,11 +244,9 @@ menu.addEventListener("click", (selectedArea) => {
       default:
         break;
     }
-  */
-  //console.log(selected);
-});
 
-/* 
+
+
 favButton.addEventListener("click", (e) => {
   favButton.classList.toggle("fav");
   favButton.classList.toggle("fav-clicked");
